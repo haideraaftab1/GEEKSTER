2a4cff2b42dd49bfbc2fdc83f13e62e27f60079a
haideraaftab1 <154618454+haideraaftab1@users.noreply.github.com>
2025-03-21 00:56:28 +0530

    first JS

---
+++ main.js
@@ -0,0 +1,24 @@
+// let a= 10;
+// a = 20;
+
+// console.log(a);
+
+
+// // const d = 10;
+// // d = 20;
+
+// // console.log(d);
+
+
+// var e = 10;
+// var e = 20;
+
+// console.log(e);
+
+
+let a = 20;
+let b = 30;
+let c = 10;
+
+console.log(--a + ++b + c-- + b + --c +--a + --b + c + --c + --a + --b + c + --c + --a + --b + c);
+
