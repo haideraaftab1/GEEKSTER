ff26cb86af33cf9869fe31dc450e17df96b0f6fb
haideraaftab1 <154618454+haideraaftab1@users.noreply.github.com>
2025-03-26 14:04:20 +0530

    string

---
+++ Intro to string/main.js
@@ -0,0 +1,17 @@
+function sum() {
+    let a = 10;
+    let b = 20;
+    let c = 30;
+    console.log(a + b + c);
+    
+}
+sum();
+
+if(true) {
+    let aa = 10;
+    console.log(aa);
+}
+
+let a = 10;
+
+
